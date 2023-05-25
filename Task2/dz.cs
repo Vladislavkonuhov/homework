51dc8177e256e25cd747728b7f67ce88ff37b2c8
Name <vkonohov19@gmail.com>
2023-05-25 16:12:33 +0300

    done task4 and 2

---
+++ Task2/dz.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+int n1 = (0);
+if (n >=100 && n < 1000){
+    n1 = (n / 10);
+    Console.WriteLine(n1 % 10);}
+else
+    Console.WriteLine("no");
+
+
+
+
+
