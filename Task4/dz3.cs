6f536477bcfab3f164cecff77e73b2e8f34a3851
Name <vkonohov19@gmail.com>
2023-05-25 16:23:02 +0300

    done task1 and task2

---
+++ Task4/dz3.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+if (n == 6 || n == 7 )
+    Console.WriteLine("Yes");
+else
+    Console.WriteLine("no");
