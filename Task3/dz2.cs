1af2eba39360e06e5b3a3393b30349378e892acc
Name <vkonohov19@gmail.com>
2023-05-26 14:57:16 +0300

    done 2 and 3

---
+++ Task3/dz2.cs
@@ -0,0 +1,10 @@
+﻿// Для 5 строчки пришлось залесть в интернет, так как незнал как конвертировать int в string)
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+string n1 = Convert.ToString(n);
+if (n1.Length > 2)
+  Console.WriteLine(n1[2]);
+else 
+  Console.WriteLine("третьей цифры нет");
+
